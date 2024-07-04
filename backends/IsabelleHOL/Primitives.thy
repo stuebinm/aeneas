bd4c7558b96b0616023b4e4600a73a82ab2a9a3d
stuebinm <stuebinm@disroot.org>
2024-07-04 16:23:25 +0200

    isabelle: more correct u32 type
    (I'd have to figure out how to do the code equation stuff to make this
    type actually usable the way nat is, but it should behave correctly in
    proofs)

--- backends/IsabelleHOL/Primitives.thy
+++ backends/IsabelleHOL/Primitives.thy
@@ -3,11 +3,17 @@ theory Primitives imports
  "HOL-Library.Monad_Syntax"
 begin
 
+typedef u32 = "{n. (n :: nat) \<le> 4294967295}"
+  morphisms Rep_u32 Abs_u32
+proof
+  show "0 \<in> {n. (n :: nat) \<le> 4294967295}" by simp
+qed
 
-datatype u32 = u32 nat
+definition u32_lt :: "u32 \<Rightarrow> u32 \<Rightarrow> bool" where
+ "u32_lt a b \<equiv> Rep_u32 a < Rep_u32 b"
+
+
 datatype 'a result = Ok 'a | Err
-fun u32_lt :: "u32 \<Rightarrow> u32 \<Rightarrow> bool" where
-  "u32_lt (u32 a) (u32 b) = (a < b)"
 
 definition bind :: "'a result \<Rightarrow> ('a \<Rightarrow> 'b result) \<Rightarrow> 'b result" where
  "bind m f \<equiv> (case m of Err \<Rightarrow> Err | Ok a \<Rightarrow> f a)"
@@ -15,5 +21,7 @@ definition bind :: "'a result \<Rightarrow> ('a \<Rightarrow> 'b result) \<Right
 adhoc_overloading Monad_Syntax.bind bind
 
 declare bind_def[simp]
+
+(* TODO: instantiation to make u32 usable with literal numbers etc. *)
 
 end
